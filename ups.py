ecb9eef9ce24d582d1f050fb15ab430caa782318
Mihai Cindea <mihai.cindea@patchman.co>
2019-09-24 10:31:52 +0200

    Refactored the code a bit and added types and removal of some keys after I got inspired from https://github.com/atribe/apcupsd-influxdb-exporter

--- ups.py
+++ ups.py
@@ -20,6 +20,9 @@ nut_password = os.getenv('NUT_USERNAME', None)
 # Other vars
 interval = float(os.getenv('INTERVAL', 21))
 ups_alias = os.getenv('UPS_ALIAS', 'UPS')
+verbose = os.getenv('VERBOSE', 'false').lower()
+remove_these_keys = ['driver.version.internal', 'driver.version.usb', 'ups.beeper.status', 'driver.name', 'battery.mfr.date']
+tag_keys = ['battery.type', 'device.model', 'device.serial', 'driver.version', 'driver.version.data', 'device.mfr', 'device.type', 'ups.mfr', 'ups.model', 'ups.productid', 'ups.serial', 'ups.vendorid']
 
 print("Connecting to InfluxDB host:{}, DB:{}".format(host, dbname))
 
@@ -36,35 +39,71 @@ if os.getenv('VERBOSE', 'false').lower() == 'true':
     print("INFLUXDB_HOST: ", host)
     print("UPS_ALIAS", ups_alias)
     print("INTERVAL: ", interval)
-    print("VERBOSE: ", os.getenv('VERBOSE', 'false'))
+    print("VERBOSE: ", verbose)
+
+
+def convert_to_type(s):
+    """ A function to convert a str to either integer or float. If neither, it will return the str. """
+    try:
+        int_var = int(s)
+        return int_var
+    except ValueError:
+        try:
+            float_var = float(s)
+            return float_var
+        except ValueError:
+            return s
+
+
+def construct_object(data, remove_these_keys, tag_keys):
+    """
+    Constructs NUT data into  an object that can be sent directly to InfluxDB
+
+    :param data: data received from
+    :param remove_these_keys:
+    :param tag_keys:
+    :return:
+    """
+    fields = {}
+    tags = {'host': os.getenv('HOSTNAME', 'localhost')}
+
+    for k, v in data.items():
+        if k not in remove_these_keys:
+            if k in tag_keys:
+                tags[k] = v
+            else:
+                fields[k] = convert_to_type(v)
+
+    result = [
+        {
+            'measurement': 'ups_status',
+            'fields': fields,
+            'tags': tags
+        }
+    ]
+    return result
+
 
 while True:
     try:
-        data = ups_client.list_vars(ups_alias)
+        ups_data = ups_client.list_vars(ups_alias)
     except:
+        tb = traceback.format_exc()
+        if verbose == 'true':
+            print(tb)
         print("Error getting data from NUT")
 
-    watts = float(os.getenv('WATTS', data.get('ups.realpower.nominal', 0.0))) * 0.01 * float(data.get('ups.load', 0.0))
-
-    json_body = [
-        {
-            'measurement': 'ups_status',
-            'fields': data,
-            'tags': {
-                'host': os.getenv('HOSTNAME', 'localhost'),
-                'ups_alias': ups_alias,
-            }
-        }
-    ]
+    json_body = construct_object(ups_data, remove_these_keys, tag_keys)
 
     try:
-        if os.getenv('VERBOSE', 'false').lower() == 'true':
+        if verbose == 'true':
             print(json_body)
             print(client.write_points(json_body))
         else:
             client.write_points(json_body)
     except:
-        if os.getenv('VERBOSE', 'false').lower() == 'true':
-            print(traceback.format_exc())
+        tb = traceback.format_exc()
+        if verbose == 'true':
+            print(tb)
         print("Error connecting to InfluxDB.")
     time.sleep(interval)
